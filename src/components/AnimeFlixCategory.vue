c4764dc73980973b25612c1b8fe438b2dc09603b
Mário André <mario_andre.l.a@hotmail.com>
2021-01-19 11:27:10 -0300

    used vue-owl-carousel and created a Category component to show the anime list in a carousel showcase

---
+++ src/components/AnimeFlixCategory.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="animeCategories">
+    <div class="titleAnime">
+      <h2>{{ category }}</h2>
+    </div>
+
+    <div class="carousel">
+      <Carousel :items="5" :margin="5" :loop="true" :nav="false">
+        <img
+          @click="goToPage(`${category}`, `${anime.id}`)"
+          class="box-filme"
+          v-for="anime in animes"
+          v-bind:key="anime.id"
+          v-bind:src="anime.image"
+          alt=""
+        />
+      </Carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from "vue-owl-carousel";
+
+export default {
+  components: {
+    Carousel,
+  },
+  props: ["category", "animes"],
+  data() {
+    return {};
+  },
+  methods: {
+    goToPage(value1, value2) {
+      this.$store.dispatch("changeInfoAnimeMain", {
+        category: value1,
+        id: value2,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.carousel {
+  margin-top: 5px;
+}
+
+.animeCategories .titleAnime {
+  background-color: rgb(67, 75, 179);
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 3px solid rgb(36, 36, 36);
+}
+.animeCategories h2 {
+  background-color: #fff;
+  color: rgb(67, 75, 179);
+  padding: 2px 10px;
+  border-radius: 4px;
+}
+
+.box-filme {
+  opacity: 0.7;
+}
+.box-filme:hover {
+  opacity: 1;
+}
+</style>
+
--- src/pages/AnimeFlixMain.vue
+++ src/pages/AnimeFlixMain.vue
@@ -2,22 +2,37 @@
     <div>
         <Header /> 
         <AnimeMain /> 
+        <Category 
+            v-for="category in categoriesAnime" 
+            v-bind:category="category.category" 
+            v-bind:key="category.id" 
+            v-bind:animes="category.animes" 
+        />
     </div>
 </template>
 
 <script>
 import Header from './../components/AnimeFlixHeader'
 import AnimeMain from './../components/AnimeFlixAnimeMain'
+import Category from './../components/AnimeFlixCategory'
+
+import { mapGetters } from 'vuex'
 
 export default {
     components: {
         Header,
-        AnimeMain
+        AnimeMain,
+        Category
     },
     data() {
         return {
            
         }
+    },
+    computed: {
+        ...mapGetters({
+            categoriesAnime: 'getCategoriesAnime'  
+        })
     }
 }
 </script>
